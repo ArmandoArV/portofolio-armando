ab9dfc3800fbf9be00d978f90a90c9ad47edc1ca
ArmandoAV <armando.arredondo.valle@gmail.com>
2023-07-14 13:14:56 -0600

    Modified the image type

--- src/app/Constants.tsx
+++ src/app/Constants.tsx
@@ -1,5 +1,7 @@
 import Winning from "./assets/Projects/Winning.png";
 import Planty from "./assets/Projects/Planty.png";
+import { StaticImageData } from "next/image";
+
 export interface NavBarElement {
   ref: string;
   class: string;
@@ -10,17 +12,17 @@ export interface AboutMe {
   description: string;
 }
 
-export interface Projects {
+export interface Project {
   title: string;
   description: string;
-  image: string;
+  image: StaticImageData;
   repo?: string;
 }
 
 export const aboutMe: AboutMe = {
   name: "Armando Arredondo Valle",
   description:
-    "I am currently studying Computer Science and have developed expertise in a range of programming languages, including C++, Python, React.js, and MySQL. Through participating in the NASA Human Rover Exploration Challenge and the Mexican International Conference of Artificial Intelligence, I have gained valuable experience in data analysis, teamwork, and leadership. I am highly motivated, dedicated, and passionate about programming, which makes me a strong candidate for opportunities in the field",
+    "I am currently studying Computer Science and have developed expertise in a range of programming languages, including C++, Python, React.js, and MySQL. Through participating in the NASA Human Rover Exploration Challenge and the Mexican International Conference of Artificial Intelligence, I have gained valuable experience in data analysis, teamwork, and leadership. I am highly motivated, dedicated, and passionate about programming, which makes me a strong candidate for opportunities in the field.",
 };
 
 export const navBarElements: Record<string, NavBarElement> = {
@@ -49,19 +51,20 @@ export const navBarElements: Record<string, NavBarElement> = {
     class: "Navbar-item",
   },
 };
-export const projects: Projects[] = [
+
+export const projectList: Project[] = [
   {
     title: "EdTech Start Up - Responsive Web Service and Unity Game",
     description:
-    "Built responsive web service for EdTech Start Up. Demonstrated full-stack web development and project management. Designed Unity game in C# for programming practice.",
-        image: Winning,
-    repo: "https://github.com/ArmandoArV/AulifyFront-End"
+      "Built responsive web service for EdTech Start Up. Demonstrated full-stack web development and project management. Designed Unity game in C# for programming practice.",
+    image: Winning,
+    repo: "https://github.com/ArmandoArV/AulifyFront-End",
   },
   {
     title: "Planty - IoT Project",
     description:
       "Created an intelligent plant pot that tracked growth variables. Utilized NodeMCU Wifi to post sensor measurements to AWS RDS. Developed ReactJs frontend for real-time monitoring and watering on the Planty website.",
     image: Planty,
-    repo: "https://github.com/TonyNogueron/react-plants"
+    repo: "https://github.com/TonyNogueron/react-plants",
   },
 ];
--- src/app/Containers/ProjectsContainer/ProjectsContainer.tsx
+++ src/app/Containers/ProjectsContainer/ProjectsContainer.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { projects } from "@/app/Constants";
+import { projectList } from "@/app/Constants";
 import ProjectsCard from "@/app/Components/ProjectsCard/ProjectsCard";
+import { StaticImageData } from "next/image";
 
 const ProjectsContainer: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1 className="text-4xl font-bold text-center text-white">Projects</h1>
       <div className="flex md:flex-row flex-col w-full max-w-screen-lg px-25 mt-5">
-        {projects.map((project) => (
+        {projectList.map((project) => (
           <ProjectsCard
             key={project.title}
             title={project.title}
             description={project.description}
-            image={project.image}
+            image={project.image.toString()} // Convert StaticImageData to string
           />
         ))}
       </div>
