5f8b116bbb7f3a5b3378268900363da94e23a410
ArmandoAV <armando.arredondo.valle@gmail.com>
2023-07-23 16:37:00 -0600

    a

--- src/app/Components/Navbar/NavBar.tsx
+++ src/app/Components/Navbar/NavBar.tsx
@@ -49,23 +49,7 @@ export default function NavBar() {
           )
         )}
       </ul>
-      <button onClick={toggleMenu}>
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          {isMenuOpen ? (
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z"
-            />
-          ) : (
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M4 5h16v2H4V5zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
-            />
-          )}
-        </svg>
-      </button>
+    
     </div>
   );
 }
